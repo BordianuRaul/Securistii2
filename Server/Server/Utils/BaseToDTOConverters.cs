e4a08b2b5454ae6551bcb8b01a589a6db7cf12f7
AchiteiCosmin <cosmin.achitei@stud.ubbcluj.ro>
2024-05-20 18:26:57 +0300

    Assignment 6 model

---
+++ Server/Server/Utils/BaseToDTOConverters.cs
@@ -0,0 +1,10 @@
+﻿using Server.DTOs;
+using Server.Models;
+
+namespace Server.Utils
+{
+    public class BaseToDTOConverters
+    {
+        public static LocationDTO Converter_LocationToDTO(Location location) => new LocationDTO { Id = location.Id, Name = location.Name, Latitude = location.Latitude, Longitude = location.Longitude };
+    }
+}
---
+++ Server/Server/Utils/DTOToBaseConverters.cs
@@ -0,0 +1,10 @@
+﻿using Server.DTOs;
+using Server.Models;
+
+namespace Server.Utils
+{
+    public class DTOToBaseConverters
+    {
+        public static Location Converter_DTOToLocation(LocationDTO locationDTO) => new Location { Id = locationDTO.Id, Name = locationDTO.Name, Latitude = locationDTO.Latitude, Longitude = locationDTO.Longitude };
+    }
+}
