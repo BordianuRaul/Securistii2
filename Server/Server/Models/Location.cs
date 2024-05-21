8c45b4b9f902e9d28bd70dec810be0320d4037c1
AchiteiCosmin <cosmin.achitei@stud.ubbcluj.ro>
2024-05-21 08:43:19 +0300

    Modified files so they abide to the StyleCop ruleset

--- Server/Server/Models/Location.cs
+++ Server/Server/Models/Location.cs
@@ -7,7 +7,7 @@ namespace Server.Models
     {
         [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)] public int Id { get; set; }
         [Required] public string Name { get; set; } = string.Empty;
-        [Required] public double Latitude{ get; set; }
+        [Required] public double Latitude { get; set; }
         [Required] public double Longitude { get; set; }
     }
 }
