20b50a4d52581d5dd2a2a40196ba2c1c1935c8d9
Boldaș Daniel-Alexandru <alexboldas@gmail.com>
2024-05-23 23:29:56 +0300

    Post id's changed to ints implemented PostReported REST api's

--- Server/Server/Utils/BaseToDTOConverters.cs
+++ Server/Server/Utils/BaseToDTOConverters.cs
@@ -13,5 +13,7 @@ namespace Server.Utils
         public static PostSavedDTO Converter_PostSavedToDTO(PostSaved postSaved) => new PostSavedDTO { save_id = postSaved.save_id, post_id = postSaved.post_id, user_id = postSaved.user_id };
 
         public static PostDTO Converter_PostToDTO(Post post) => new PostDTO { Post_Id = post.Post_Id, Owner_User_Id = post.Owner_User_Id, Description = post.Description, Commented_Post_Id = post.Commented_Post_Id, Original_Post_Id = post.Original_Post_Id, Media_Path = post.Media_Path, Post_Type = post.Post_Type, Location_Id = post.Location_Id, Created_Date = post.Created_Date };
+    
+        public static PostReported Converter_PostReportedDTO(PostReportedDTO postReportedDTO) => new PostReported { Report_Id = postReportedDTO.Report_Id, Reason = postReportedDTO.Reason, Description = postReportedDTO.Description, Post_Id = postReportedDTO.Post_Id, Reporter_Id = postReportedDTO.Reporter_Id };
     }
 }
